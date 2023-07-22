a9a08bf49c5e74c5ddd36dadfa270943ab78ca5a
KareemE125 <kareemezzat.125@gmail.com>
2023-07-22 16:54:36 +0200

    Project Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,24 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: 'hsl(172, 67%, 45%)',
+        'neutral-900': 'hsl(183, 100%, 15%)',
+        'neutral-600': 'hsl(186, 14%, 43%)',
+        'neutral-400': 'hsl(184, 14%, 56%)',
+        'neutral-200': 'hsl(185, 41%, 84%)',
+        'neutral-100': 'hsl(189, 41%, 97%)',
+        white: 'hsl(0, 0%, 100%)',
       },
+      fontFamily: {
+        'space-mono': ['Space Mono', 'monospace'],
+      },
+      fontSize: {
+        'form-input': '24px',
+      },
+    },
+    screens: {
+      'mobile': {'max': '640px'},
     },
   },
   plugins: [],
